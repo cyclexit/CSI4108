38a034f7bb688abe22d713bb613aa321df8c1a28
cyclexit <cyclexit@outlook.com>
2022-10-16 23:30:10 -0400

    Implement the encryption utilities

--- diff_analysis_person_1.py
+++ diff_analysis_person_1.py
@@ -1,4 +1,11 @@
-MAX_VALUE = 1 << 16 - 1
+# constants
+NUM_OF_BITS = 16
+MAX_VALUE = 1 << NUM_OF_BITS - 1
+PLAINTEXT_NUM = 10000
+
+# variables
+round_keys = [55382, 42954, 53122, 38368, 40273]
+
 s_box = {
     0x0: 0xb,
     0x1: 0x7,
@@ -17,8 +24,19 @@ s_box = {
     0xe: 0x0,
     0xf: 0x3,
 }
-round_keys = [55382, 42954, 53122, 38368, 40273]
-pc_pairs = dict()
+
+def permutation(bit_str_16: str):
+    assert(len(bit_str_16) == NUM_OF_BITS)
+    res = list(bit_str_16)
+    for i in range(0, NUM_OF_BITS):
+        # print(4 * (i % 4) + (i // 4)) # debug
+        res[4 * (i % 4) + (i // 4)] = bit_str_16[i]
+    # print(bit_str_16) # debug
+    # print(''.join(res)) # debug
+    return ''.join(res)
 
 if __name__ == "__main__":
+    num_bits = 16
+    num = 55382
+    permutation(f"{num:0{num_bits}b}")
     pass
