abdc85f155a4a2344a37d0c220567f84fd1f642c
cyclexit <cyclexit@outlook.com>
2022-10-17 09:38:57 -0400

    Implement the one-round encrypt function

--- diff_analysis_person_1.py
+++ diff_analysis_person_1.py
@@ -1,7 +1,10 @@
+import random
+
 # constants
 NUM_OF_BITS = 16
 MAX_VALUE = 1 << NUM_OF_BITS - 1
 PLAINTEXT_NUM = 10000
+ENCRYPTION_ROUNDS = 4 # NOTE: round 5 is simply key mixing
 
 # variables
 round_keys = [55382, 42954, 53122, 38368, 40273]
@@ -25,7 +28,7 @@ s_box = {
     0xf: 0x3,
 }
 
-def permutation(bit_str_16: str):
+def permutation(bit_str_16: str) -> str:
     assert(len(bit_str_16) == NUM_OF_BITS)
     res = list(bit_str_16)
     for i in range(0, NUM_OF_BITS):
@@ -35,8 +38,39 @@ def permutation(bit_str_16: str):
     # print(''.join(res)) # debug
     return ''.join(res)
 
+def encrypt_one_round(to_encrypt: int, k: int) -> str:
+    # key mixing
+    # print(to_encrypt, k) # debug
+    to_encrypt ^= k
+    # print(to_encrypt) # debug
+
+    # 4-bit s-box
+    PART_NUM_OF_BITS = 4
+    NUM_OF_PARTS = NUM_OF_BITS / PART_NUM_OF_BITS
+    parts = [] # index 0: least significant
+    while to_encrypt > 0:
+        parts.append(to_encrypt % (1 << PART_NUM_OF_BITS))
+        to_encrypt //= 16
+    while len(parts) < 4:
+        parts.append(0)
+    assert(len(parts) == NUM_OF_PARTS)
+    parts.reverse()
+    # print("before s-box:", parts) # debug
+    for i in range(0, len(parts)):
+        parts[i] = s_box[parts[i]]
+    # print("after s-box:", parts) # debug
+    bit_str_16 = ''.join(f"{x:0{PART_NUM_OF_BITS}b}" for x in parts)
+    # print(bit_str_16) # debug
+    
+    # permutation
+    res = permutation(bit_str_16)
+    # print(res) # debug
+    return res
+
 if __name__ == "__main__":
     num_bits = 16
     num = 55382
-    permutation(f"{num:0{num_bits}b}")
+    print(permutation(f"{num:0{num_bits}b}"))
+
+    encrypt_one_round(798, round_keys[0])
     pass
