3bd9f589fe1592ab03eb26c142d4f374d4984639
cyclexit <cyclexit@outlook.com>
2022-11-01 16:19:21 -0400

    Make some progress in q1

--- a3/q1.py
+++ a3/q1.py
@@ -3,17 +3,33 @@
 '''
 import random
 
-q = 89
-alpha = 13
+def fast_pow(x, y, n):
+    res = 1;
+    x = x % n;
+    while y > 0:
+        if y & 1:
+            res = (res * x) % n
+        y >>= 1
+        x = (x * x) % n
+    return res
+
+q = 89 # (given)
+alpha = 13 # (given)
 
 # private key
-xa = random.randint(2, q - 2)
+xa = 12 # random.randint(2, q - 2)
 
 # public key
-ya = (alpha ** xa) % q
+ya = fast_pow(alpha, xa, q)
 
 def encrypt(q, alpha, ya):
     pass
 
 def decrypt(xa):
     pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
