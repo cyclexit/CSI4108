7febc3c40e595cd29efa3ae1f80a6df28371fdbf
cyclexit <cyclexit@outlook.com>
2022-11-05 13:41:14 -0400

    Init q3b

---
+++ a3/q3b.py
@@ -0,0 +1,12 @@
+# 256-bit prime number generated on https://asecuritysite.com/encryption/getprimen
+p = 60457697227393588458965428400547629570836522862050333268997311092825458108667
+
+# For the simplicity, choose a = b = 1
+a = 1
+b = 1
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
