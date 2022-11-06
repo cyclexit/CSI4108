58e45cd77d59d52042abce4852c8ddc7728afa9e
cyclexit <cyclexit@outlook.com>
2022-11-05 23:36:14 -0400

    Fix bug

--- a3/q3b.py
+++ a3/q3b.py
@@ -3,14 +3,17 @@
     1. https://asecuritysite.com/encryption/ecdh3
 '''
 import collections
+import time
+from random import randint
 
+# Copied code starts
 EllipticCurve  = collections.namedtuple("EllipticCurve", ["p", "a", "b", "g", "n", "h"])
 curve = EllipticCurve(
     p = 115792089210356248762697446949407573530086143415290314195533631308867097853951,
     a = -3,
-    b = 0xb4050a850c04b3abf54132565044b0b7d7bfd8ba270b39432355ffb4,
-    g = (0xb70e0cbd6bb4bf7f321390b94a03c1d356c21122343280d6115c1d21, 0xbd376388b5f723fb4c22dfe6cd4375a05a07476444d5819985007e34),
-    n = 26959946667150639794667015087019625940457807714424391721682722368061,
+    b = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b,
+    g = (0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296,0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5),
+    n = 115792089210356248762697446949407573529996955224135760342422259061068512044369,
     h = 1,
 )
 
@@ -119,8 +122,27 @@ def scalar_mult(k, point):
     assert is_on_curve(result)
 
     return result
+# Copied code ends
+
+def ecdh_key_gen():
+    # Alice's key pair
+    na = randint(1, curve.n) # private
+    pa = scalar_mult(na, curve.g) # public
+    # Bob's key pair
+    nb = randint(1, curve.n) # private
+    pb = scalar_mult(nb, curve.g) # public
+
+    # Alice's shared secret
+    ka = scalar_mult(na, pb)
+    # Bob's shared secret
+    kb = scalar_mult(nb, pa)
+    return na, pa, nb, pb, ka, kb
 
 def main():
+    print(curve)
+    start_time = time.time_ns()
+    na, pa, nb, pb, ka, kb = ecdh_key_gen()
+    print(f"ECDH Time: {(time.time_ns() - start_time)} ns")
     pass
 
 if __name__ == "__main__":
