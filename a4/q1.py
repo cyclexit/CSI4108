583aadcacaa03b2be911ed854278e47df924b7b2
cyclexit <cyclexit@outlook.com>
2022-11-19 15:16:54 -0500

    Result matched!!!

--- a4/q1.py
+++ a4/q1.py
@@ -6,29 +6,29 @@ OUTPUT_BYTE_LEN = OUTPUT_BIT_LEN // 8
 def byte_xor(a: bytes, b: bytes) -> bytes:
     return (int.from_bytes(a, 'little') ^ int.from_bytes(b, 'little')).to_bytes(max(len(a), len(b)), 'little')
 
-def my_hmac(key: bytes, msg: bytes) -> bytes:
+def my_hmac(key: bytes, msg: bytes):
     IPAD = 0x36
     OPAD = 0x5c
     full_ipad = 0
     full_opad = 0
-    for _ in range(0, OUTPUT_BYTE_LEN):
+    for _ in range(0, 128):
         full_ipad = (full_ipad << 8) + IPAD
         full_opad = (full_opad << 8) + OPAD
-    full_ipad = full_ipad.to_bytes(OUTPUT_BYTE_LEN, "little")
-    full_opad = full_opad.to_bytes(OUTPUT_BYTE_LEN, "little")
+    full_ipad = full_ipad.to_bytes(128, "little")
+    full_opad = full_opad.to_bytes(128, "little")
     if len(key) < 128:
         key = key + b'\x00' * (128 - len(key))
-    inner = hashlib.sha512(byte_xor(key, full_ipad) + msg).digest()
-    outer = hashlib.sha512(byte_xor(key, full_opad) + inner).digest()
+    inner = hashlib.sha512(byte_xor(key, full_ipad) + msg)
+    outer = hashlib.sha512(byte_xor(key, full_opad) + inner.digest())
     return outer
 
 def main():
     message = b"I am using this input string to test my own implementation of HMAC-SHA-512."
     key = b"This is my simple key."
     my_res =  my_hmac(key, message)
-    print(len(my_res), my_res)
-    truth = hmac.HMAC(key, message, digestmod=hashlib.sha512).digest()
-    print(len(truth), truth)
+    print(my_res.block_size, my_res.digest())
+    truth = hmac.HMAC(key, message, digestmod=hashlib.sha512)
+    print(truth.block_size, truth.digest())
 
 if __name__ == "__main__":
     main()
