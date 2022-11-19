bce2435f59b66d59e0357f165348da1e464c3bc5
cyclexit <cyclexit@outlook.com>
2022-11-19 14:44:05 -0500

    First implement my hmac

--- a4/q1.py
+++ a4/q1.py
@@ -3,7 +3,13 @@ import hashlib, hmac
 OUTPUT_BIT_LEN = 512
 OUTPUT_BYTE_LEN = OUTPUT_BIT_LEN // 8
 
-def my_hmac(key: bytes, msg: bytes):
+def byte_xor(a: bytes, b: bytes) -> bytes:
+    return (int.from_bytes(a, 'big') ^ int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+
+def byte_or(a: bytes, b: bytes) -> bytes:
+    return (int.from_bytes(a, 'big') | int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+
+def my_hmac(key: bytes, msg: bytes) -> bytes:
     IPAD = 0x36
     OPAD = 0x5c
     full_ipad = 0
@@ -11,16 +17,19 @@ def my_hmac(key: bytes, msg: bytes):
     for _ in range(0, OUTPUT_BYTE_LEN):
         full_ipad = (full_ipad << 8) + IPAD
         full_opad = (full_opad << 8) + OPAD
-    # res = hashlib.sha512((key ^ full_ipad) | msg)
-    # print(len(hex(full_ipad)), hex(full_ipad)) # debug
-    # print(full_ipad.to_bytes(OUTPUT_BYTE_LEN, "big")) # debug
-    # print(len(hex(full_opad)), hex(full_opad)) # debug
-    # print(full_opad.to_bytes(OUTPUT_BYTE_LEN, "big")) # debug
+    full_ipad = full_ipad.to_bytes(OUTPUT_BYTE_LEN, "big")
+    full_opad = full_opad.to_bytes(OUTPUT_BYTE_LEN, "big")
+    if len(key) < OUTPUT_BYTE_LEN:
+        key = key + b'\x00' * (OUTPUT_BYTE_LEN - len(key))
+    inner = hashlib.sha512(byte_or(byte_xor(key, full_ipad), msg)).digest()
+    outer = hashlib.sha512(byte_or(byte_xor(key, full_opad), inner)).digest()
+    return outer
 
 def main():
     message = b"I am using this input string to test my own implementation of HMAC-SHA-512."
     key = b"This is my simple key."
-    my_hmac(key, message)
+    print(my_hmac(key, message))
+    print()
 
 if __name__ == "__main__":
     main()
