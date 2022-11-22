59f9cee8bf4d61eaa77b65eaa7e40b90ad572f55
cyclexit <cyclexit@outlook.com>
2022-11-22 16:08:01 -0500

    Make the change

--- a4/q2_and_q3.py
+++ a4/q2_and_q3.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     q2_x, q2_y = dsa_inst.key_gen()
     q2_m = 522346828557612 # given
     q2_r, q2_s = dsa_inst.sign(q2_m, q2_x)
-    q2_verified = dsa_inst.verify(q2_r, q2_s, q2_y)
+    q2_verified = dsa_inst.verify(q2_m, q2_r, q2_s, q2_y)
     if q2_verified:
         print("Question 2 signature is verified!")
     else:
