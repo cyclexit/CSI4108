a53f38ac434a793681ed7c9adb92480c69823bdc
cyclexit <cyclexit@outlook.com>
2022-11-22 15:56:16 -0500

    Make some progress

--- a4/dsa.py
+++ a4/dsa.py
@@ -13,7 +13,7 @@ class Dsa:
         self.p = p
         self.q = q
         self.g = g
-        self.hash = hash
+        self.hash = hash()
         self.update_k_and_r()
 
     def key_gen(self):
@@ -24,17 +24,24 @@ class Dsa:
     def update_k_and_r(self):
         self.k = randint(1, self.q - 1)
         self.k_inv, _ = extgcd(self.k, self.q)
+        self.k_inv %= self.q
         self.r = fast_pow(self.g, self.k, self.p) % self.q
 
     def sign(self, m: int, private_key):
-        m_hash = int(self.hash(m.to_bytes(self.q.bit_length(), 'big')).hexdigest(), 16)
+        self.hash.update(m.to_bytes(self.q.bit_length(), 'big'))
+        m_hash = int(self.hash.hexdigest(), 16)
         s = ((m_hash + private_key * self.r) * self.k_inv) % self.q
         return (self.r, s)
 
     def verify(self, r, s, public_key):
-        s_hash = int(self.hash(s.to_bytes(self.q.bit_length(), 'big')).hexdigest(), 16)
+        self.hash.update(s.to_bytes(self.q.bit_length(), 'big'))
+        s_hash = int(self.hash.hexdigest(), 16)
         s_inv, _ = extgcd(s, self.q)
+        s_inv %= self.q
         u1 = (s_hash * s_inv) % self.q
         u2 = r * s_inv % self.q
         cksum = (fast_pow(self.g, u1, self.p) * fast_pow(public_key, u2, self.p)) % self.p % self.q
+        print(self.q.bit_length(), self.q)
+        print(cksum)
+        print(r)
         return cksum == r
