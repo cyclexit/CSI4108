63818e730e40bc6b53ed7cff2eda0170944789a9
cyclexit <cyclexit@outlook.com>
2022-11-22 16:03:11 -0500

    Some progress

--- a4/dsa.py
+++ a4/dsa.py
@@ -27,21 +27,18 @@ class Dsa:
         self.k_inv %= self.q
         self.r = fast_pow(self.g, self.k, self.p) % self.q
 
-    def sign(self, m: int, private_key):
+    def sign(self, m: int, private_key: int):
         self.hash.update(m.to_bytes(self.q.bit_length(), 'big'))
-        m_hash = int(self.hash.hexdigest(), 16)
+        m_hash = int(self.hash.hexdigest(), 16) % self.q
         s = ((m_hash + private_key * self.r) * self.k_inv) % self.q
         return (self.r, s)
 
-    def verify(self, r, s, public_key):
+    def verify(self, r, s, public_key: int):
         self.hash.update(s.to_bytes(self.q.bit_length(), 'big'))
-        s_hash = int(self.hash.hexdigest(), 16)
+        s_hash = int(self.hash.hexdigest(), 16) % self.q
         s_inv, _ = extgcd(s, self.q)
         s_inv %= self.q
         u1 = (s_hash * s_inv) % self.q
         u2 = r * s_inv % self.q
         cksum = (fast_pow(self.g, u1, self.p) * fast_pow(public_key, u2, self.p)) % self.p % self.q
-        print(self.q.bit_length(), self.q)
-        print(cksum)
-        print(r)
         return cksum == r
